dcf8b8067cf81fc5c68a81efa02444b47a5f4ae8
Budda Lee <buddalee1stlove@gmail.com>
2018-10-07 23:06:54 +0800

    目前可以show 出圖片正確位置和旋轉，但旋轉方向還不知道，手機版也無法touch但可以click

--- js/click.js
+++ js/click.js
@@ -1,5 +1,21 @@
-var app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
+
+var app = new PIXI.Application(1920, 1080, { backgroundColor: 0x1099bb });
+
+
 document.body.appendChild(app.view);
+// var texture = PIXI.Texture.fromImage('assets/1.png');
+// var bunny = new PIXI.Sprite(texture);
+// bunny.x = app.screen.width / 2;
+// bunny.y = app.screen.height / 2;
+// setTimeout(() => {
+//     console.log('texture.orig.width', texture.width)
+
+//     bunny.pivot.x = texture.width / 2;
+//     bunny.pivot.y = texture.height / 2;
+//     bunny.rotation = 180 * (Math.PI / 180);
+//     app.stage.addChild(bunny);
+// }, 1000);
+
 
 // Scale mode for all textures, will retain pixelation
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
@@ -21,6 +37,8 @@ PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 // sprite.on('pointerdown', onClick);
 app.stage.interactive = true;
 app.stage.buttonMode = true;
+app.renderer.plugins.interaction.autoPreventDefault = false;
+console.log('renderer', app.renderer.plugins.interaction.autoPreventDefault);
 // console.log('app.stage', app.stage);
 // app.stage.click = function(e){
 //     console.log('!!!!', e.data.global);
@@ -37,15 +55,38 @@ app.stage
     .on('pointerdown', onDragStart)
     .on('pointerup', onDragEnd)
     .on('pointerupoutside', onDragEnd);
-
+// app.stage
+//     .on('touchstart', onDragStart)
+//     .on('touchend', onDragEnd)
+//     .on('touchendoutside', onDragEnd);
+var isShowDot = false;
+if (location.search === '?dot') {
+    isShowDot = true;
+}
+function showDot() {
+    isShowDot = true;
+}
 function onDragStart(e) {
-    this.dragging = true;
     this.data = e.data;
+
+    // if (this.data.identifier !== 'MOUSE') {
+
+    // alert('!!!!!');
+    // console.log('e: ', e.data);
+    this.dragging = true;
     var dotNum = app.stage.children.length - 1;
-    console.log('dotNum', dotNum);
-    if (this.data.identifier === 0 && dotNum > 0) {
+    // console.log('dotNum', dotNum);
+    console.log('this.data.identifier: ', this.data.identifier);
+    if (this.data.identifier === 0 && dotNum > 0) { // debug pc
+        // if ((touches.findIndex(_touch => _touch.id === this.data.identifier)) > -1 && dotNum > 0) {
+
+        console.log('~~~');
         if (dotNum > 2) {
-            app.stage.removeChild(app.stage.children[dotNum]);
+            // app.stage.removeChild(bunny);
+
+            app.stage.removeChild(app.stage.children[dotNum]); // 把印章刪掉
+            app.stage.removeChild(app.stage.children[dotNum - 1]); // 把中心點去除
+
         }
 
         touches.forEach(_num => {
@@ -65,15 +106,20 @@ function onDragStart(e) {
     };
     // console.log('touch: ', touch);
     var testCircle = new PIXI.Graphics();
-    testCircle.beginFill(0xff0000);
+    if (isShowDot) {
+        testCircle.beginFill(0xff0000); 
+     } else {
+        testCircle.beginFill(0xff0000, 0); 
+     }
     testCircle.drawCircle(touch.pos.x, touch.pos.y, 10);
     testCircle.endFill();
     app.stage.addChild(testCircle);
     // console.log('touch: ', touch);
     touches.push(touch);
+    touchHandler();
+    // }
     // console.log('touches: ', touches);
 
-    touchHandler();
 }
 
 function onDragEnd(e) {
@@ -106,7 +152,7 @@ function touchHandler() {
 // 畫一個方塊
 var testGraphics = new PIXI.Graphics();
 testGraphics.beginFill(0xff0000, 0);
-testGraphics.drawRect(0, 0, 800, 600);
+testGraphics.drawRect(0, 0, 1920, 1080);
 testGraphics.endFill();
 app.stage.addChild(testGraphics);
 
@@ -133,8 +179,46 @@ function calcDistance() {
     console.log('max distance: ', maxDistance);
     const maxIdx = dis.findIndex(_dis => _dis === maxDistance);
     calcPhotoCenter(maxIdx);
-}
+    calcPhotoAngle(maxIdx);
+    renderStamp();
 
+}
+var stamp;
+function renderStamp() {
+    var texture = PIXI.Texture.fromImage('assets/1.png');
+    stamp = new PIXI.Sprite(texture);
+
+    setTimeout(() => {
+        stamp.x = centerX;
+        stamp.y = centerY;
+
+        stamp.pivot.x = texture.width / 2;
+        stamp.pivot.y = texture.height / 2;
+        stamp.rotation = angle * (Math.PI / 180);
+        app.stage.addChild(stamp);
+
+    }, 300);
+
+}
+var angle;
+function calcPhotoAngle(maxIdx) {
+    var anotherIdx;
+    if (maxIdx === 0) {
+        anotherIdx = touches.length - 1;
+    } else {
+        anotherIdx = maxIdx - 1;
+    }
+    const { x, y } = touches[maxIdx].pos;
+    const _x = touches[anotherIdx].pos.x;
+    const _y = touches[anotherIdx].pos.y;
+    var cX = _x - x;
+    var cY = _y - y;
+    var xrad = Math.atan2(cY, cX);
+    angle = xrad / Math.PI * 180;
+    angle = angle;
+    console.log('angle: ', angle);
+}
+var centerX, centerY;
 function calcPhotoCenter(maxIdx) {
     // console.log('touches', touches);
     // console.log('maxIdx: ', maxIdx);
@@ -147,12 +231,18 @@ function calcPhotoCenter(maxIdx) {
     const { x, y } = touches[maxIdx].pos;
     const _x = touches[anotherIdx].pos.x;
     const _y = touches[anotherIdx].pos.y;
-    const centerX = (x + _x) / 2;
-    const centerY = (y + _y) / 2;
+    centerX = (x + _x) / 2;
+    centerY = (y + _y) / 2;
     console.log('centerX: ', centerX);
     console.log('centerY: ', centerY);
     var centerCircle = new PIXI.Graphics();
-    centerCircle.beginFill(0x00FF00);
+    if (isShowDot) {
+       centerCircle.beginFill(0x00FF00); 
+    } else {
+       centerCircle.beginFill(0x00FF00, 0); 
+        
+    }
+    
     centerCircle.drawCircle(centerX, centerY, 10);
     centerCircle.endFill();
     app.stage.addChild(centerCircle);
